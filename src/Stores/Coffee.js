dfdf3cdf9c55f728201d18c9ed3d7c225a8f332c
Ondrej Chrastina <chrastina.ondra@gmail.com>
2017-09-01 13:28:37 +0200

    remove forgotten consol login

--- src/Stores/Coffee.js
+++ src/Stores/Coffee.js
@@ -27,7 +27,6 @@ let fetchFilterProperties = () => {
     .type('coffee')
     .get()
     .subscribe(response => {
-      console.log(response);
       processings = response
         .type
         .elements.find(element => element.codename === "processing")
