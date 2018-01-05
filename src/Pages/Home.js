9ac2958f123ae5a4c7c9860377a2b592c31959c2
Ondřej Chrastina <chrastina.ondra@gmail.com>
2018-01-05 14:31:45 +0100

    added translation of local resourcef for Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { translate } from 'react-translate'
+
 import Banner from '../Components/Banner.js';
 import LatestArticles from '../Components/LatestArticles.js';
 import LinkButton from '../Components/LinkButton.js';
 import OurStory from '../Components/OurStory.js';
 import TasteOurCoffee from '../Components/TasteOurCoffee.js';
 
+
 const Home = (props) => {
   return (
     <div className="container">
       <Banner />
       <LatestArticles language={props.language}/>
-      <LinkButton link={`/${props.language}/articles`} text="More articles" />
+      <LinkButton link={`/${props.language}/articles`} text={props.t("moreArticles")} />
       <OurStory />
-      <LinkButton link={`/${props.language}/about`} text="Read the whole story" />
+      <LinkButton link={`/${props.language}/about`} text={props.t("aboutLinkText")} />
       <TasteOurCoffee language={props.language}/>
-      <LinkButton link={`/${props.language}/cafes`} text="Find out more" />
+      <LinkButton link={`/${props.language}/cafes`} text={props.t("cafesLinkText")} />
     </div>
   );
 }
 
-export default Home;
+export default translate("Home")(Home);
