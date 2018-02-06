8654e751f275aa75f9fadbed8b227d1f8c737f42
Jennifer Pazos <jmp521@gmail.com>
2018-02-06 00:33:18 -0600

    attempted to make language changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Link from '../Components/LowerCaseUrlLink';
-class Footer extends Component {
+import { translate } from 'react-translate'
+const Footer = (props) => {
 
-  render() {
     return(
       <div className="footer-wrapper">
         <footer role="contentinfo">
@@ -13,18 +13,18 @@ class Footer extends Component {
                   <h3 className="contact-title">Contact</h3>
                   <p>
                     (+1) 617-632-4520<br />
-                    <a href="mailto:dancinggoat@localhost.local">dancinggoat@localhost.local</a><br />
+                    <Link to="mailto:dancinggoat@localhost.local">dancinggoat@localhost.local</Link><br />
                     <br /> Dancing Goat Ltd<br /> 62 E Lake St Chicago,<br /> Illinois 60601, USA
                   </p>
                 </div>
                 <div className="col-md-4 col-lg-4 footer-col">
                   <h3>Follow us</h3>
-                  <a className="followus-link" href="https://www.facebook.com/Dancing.Goat" target="_blank">
+                  <Link className="followus-link" to={"https://www.facebook.com/Dancing.Goat"} target="_blank">
                     <img alt="Follow us on Facebook" className="" src="/Images/facebook-icon.png" title="Follow us on Facebook" />
-                  </a>
-                  <a className="followus-link" href="https://twitter.com/DancingGoat78" target="_blank">
+                  </Link>
+                  <Link className="followus-link" to={"https://twitter.com/DancingGoat78"} target="_blank">
                     <img alt="Follow us on Twitter" className="" src="/Images/twitter-icon.png" title="Follow us on Twitter" />
-                  </a>
+                  </Link>
                 </div>
                 <div className="col-md-4 col-lg-4 footer-col" />
               </div>
@@ -36,6 +36,6 @@ class Footer extends Component {
         </footer>
       </div>
     )
-  }
+
 }
-export default Footer
+export default translate("Footer")(Footer);
