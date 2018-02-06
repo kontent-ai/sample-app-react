0a79a73cdeb9e3237578bbab1b0f4219ec10b449
Jennifer Pazos <jmp521@gmail.com>
2018-02-06 13:54:08 -0600

    updated the english and spanish json files and updated the footer with calls to props.t so that the correct language shows up on the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,20 +10,20 @@ const Footer = (props) => {
             <div className="container">
               <div className="row">
                 <div className="col-md-4 col-lg-4 footer-col">
-                  <h3 className="contact-title">Contact</h3>
+                  <h3 className="contact-title">{props.t("contact")}</h3>
                   <p>
                     (+1) 617-632-4520<br />
                     <Link to="mailto:dancinggoat@localhost.local">dancinggoat@localhost.local</Link><br />
-                    <br /> Dancing Goat Ltd<br /> 62 E Lake St Chicago,<br /> Illinois 60601, USA
+                    <br /> Dancing Goat Ltd<br /> 62 E Lake St Chicago,<br /> {props.t("cityStateZip")}
                   </p>
                 </div>
                 <div className="col-md-4 col-lg-4 footer-col">
-                  <h3>Follow us</h3>
+                  <h3>{props.t("followUs")}</h3>
                   <Link className="followus-link" to={"https://www.facebook.com/Dancing.Goat"} target="_blank">
-                    <img alt="Follow us on Facebook" className="" src="/Images/facebook-icon.png" title="Follow us on Facebook" />
+                    <img alt="Follow us on Facebook" className="" src="/Images/facebook-icon.png" title={props.t("followUsOnFacebook")} />
                   </Link>
                   <Link className="followus-link" to={"https://twitter.com/DancingGoat78"} target="_blank">
-                    <img alt="Follow us on Twitter" className="" src="/Images/twitter-icon.png" title="Follow us on Twitter" />
+                    <img alt="Follow us on Twitter" className="" src="/Images/twitter-icon.png" title={props.t("followUsOnTwitter")} />
                   </Link>
                 </div>
                 <div className="col-md-4 col-lg-4 footer-col" />
@@ -31,7 +31,7 @@ const Footer = (props) => {
             </div>
           </div>
           <div className="container copyright">
-            Copyright &copy; 2016 Dancing Goat. All rights reserved.
+            Copyright &copy; 2016 Dancing Goat. {props.t("allRightsReserved")}
           </div>
         </footer>
       </div>
