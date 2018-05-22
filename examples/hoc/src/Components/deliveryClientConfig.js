c523f422e5faba7aaaadd60424decb4d2c910b93
Simply007 <chrastina.ondra@gmail.com>
2018-05-22 18:08:23 +0200

    use correct methos in example and upgrade react script there also

--- examples/hoc/src/Components/deliveryClientConfig.js
+++ examples/hoc/src/Components/deliveryClientConfig.js
@@ -1,9 +1,11 @@
 import { TypeResolver } from "kentico-cloud-delivery";
 import { Article } from './ArticleModel';
 
-export default config = {
+const config = {
     projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
     typeResolvers: [
         new TypeResolver('article', () => new Article())
     ]
 }
+
+export default config;
