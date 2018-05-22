3cb71d28a099e2ac2c366b580dafc64970626fae
Simply007 <chrastina.ondra@gmail.com>
2018-05-22 13:59:30 +0200

    use href attribute for link resolution (about us link did not worked)

--- src/Components/RichTextElement.js
+++ src/Components/RichTextElement.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { resolveContentLink } from '../Utilities/ContentLinks';
 import { getLanguageCode } from '../Utilities/LanguageCodes'
 
 
@@ -8,15 +7,12 @@ function handleClick(element, history, match, e) {
   if (e.target.tagName === "A" && e.target.hasAttribute("data-item-id")) {
     e.preventDefault();
 
-    const id = e.target.getAttribute("data-item-id");
-    const link = element.links.find(m => m.itemId === id);
-
+    const link = e.target.getAttribute("href");
     if (link) {
-      const path = resolveContentLink(link);
       const language = getLanguageCode(match).toLowerCase();;
 
-      if (path) {
-        history.push(`/${language}${path}`);
+      if (language) {
+        history.push(`/${language}${link}`);
       }
     }
   }
