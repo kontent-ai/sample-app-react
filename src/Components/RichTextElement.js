b11226cac83bb6c8471ecf198f29f602536b7065
Simply007 <chrastina.ondra@gmail.com>
2018-05-23 10:02:19 +0200

    use links property from rich text element returned in v 4.0.2 of the kc sdk

--- src/Components/RichTextElement.js
+++ src/Components/RichTextElement.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { resolveContentLink } from '../Utilities/ContentLinks';
 import { getLanguageCode } from '../Utilities/LanguageCodes'
 
 
@@ -7,12 +8,15 @@ function handleClick(element, history, match, e) {
   if (e.target.tagName === "A" && e.target.hasAttribute("data-item-id")) {
     e.preventDefault();
 
-    const link = e.target.getAttribute("href");
+    const id = e.target.getAttribute("data-item-id");
+    const link = element.links.find(m => m.itemId === id);
+
     if (link) {
+      const path = resolveContentLink(link);
       const language = getLanguageCode(match).toLowerCase();;
 
-      if (language) {
-        history.push(`/${language}${link}`);
+      if (path) {
+        history.push(`/${language}${path}`);
       }
     }
   }
