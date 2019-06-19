f8b44a3385071d9ec8ff31d20ae449a2029001cd
MASTER\martinh <martinh@kentico.com>
2019-06-19 09:43:56 +0200

    DCN-151 - Prepare for Kentico repository

--- examples/hoc/src/Components/deliveryClientConfig.js
+++
@@ -1,9 +0,0 @@
-import { TypeResolver } from 'kentico-cloud-delivery';
-import { Article } from './ArticleModel';
-
-const config = {
-  projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
-  typeResolvers: [new TypeResolver('article', () => new Article())]
-};
-
-export default config;
