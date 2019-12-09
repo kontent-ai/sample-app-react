322fb557fb6b85055dbd6c7111da3396ba19eeea
Samuelly332 <42471390+Samuelly332@users.noreply.github.com>
2019-12-09 18:39:49 +0100

    fixed typo

--- src/Utilities/SelectedProject.js
+++ src/Utilities/SelectedProject.js
@@ -1,13 +1,22 @@
+import { resetClient, Client } from '../Client';
+
 const selectedProjectCookieName = 'ProjectId';
 const projectConfigurationPath = '/Admin/Configuration';
 
 const defaultProjectId = '975bf280-fd91-488c-994c-2f04416e5ee3';
 
-const SAMPLE_PROJECT_ITEM_COUNT = 30;
+const getSampleProjectItems = (projectId = defaultProjectId) => {
+  resetClient(projectId);
+
+  return Client.items()
+    .elementsParameter(['id'])
+    .depthParameter(0)
+    .getObservable();
+}
 
 export {
   selectedProjectCookieName,
   projectConfigurationPath,
   defaultProjectId,
-  SAMPLE_PROJECT_ITEM_COUNT
+  getSampleProjectItems
 };
