62b0da9260621fafd846ee9d29ffd43cbd34e668
Ondrej Chrastina <chrastins.ondra@gmail.com>
2022-03-21 13:22:52 +0100

    remove AboutUs store

--- src/Utilities/StoreManager.js
+++ src/Utilities/StoreManager.js
@@ -1,11 +1,9 @@
 import * as ArticleStore from '../Stores/Article';
-import * as AboutStore from '../Stores/About';
 import * as BrewerStore from '../Stores/Brewer';
 import * as CafeStore from '../Stores/Cafe';
 import * as CoffeeStore from '../Stores/Coffee';
 
 const allStores = [
-  AboutStore,
   ArticleStore,
   BrewerStore,
   CafeStore,
