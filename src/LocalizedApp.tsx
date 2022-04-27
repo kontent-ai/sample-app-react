1ef1f3cb0545f0070388071156b098ecec4c630b
Ivan Kiral <ivanK@kentico.com>
2022-04-27 09:33:35 +0200

    Remove unnecessary comments

--- src/LocalizedApp.tsx
+++ src/LocalizedApp.tsx
@@ -14,13 +14,6 @@ const LocalizedApp: React.FC = () => {
   const [language, setLanguage] = useState(englishCode);
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     language: getLanguageCode(props.match)
-  //   };
-  //   this.setLanguageCode = this.setLanguageCode.bind(this);
-  // }
 
   const setLanguageCode = (newLanguage: string, newUrl: string): void => {
     if (
@@ -30,7 +23,7 @@ const LocalizedApp: React.FC = () => {
       return;
     }
 
-    const urlParts = pathname.split('/'); //TODO check why there are two splits
+    const urlParts = pathname.split('/');
     const currentLanguage = pathname.split('/')[1];
     if (languageCodesLowerCase.indexOf(currentLanguage) > -1) {
       urlParts[1] = newLanguage;
@@ -40,10 +33,8 @@ const LocalizedApp: React.FC = () => {
 
     setLanguage(newLanguage);
     if (newUrl) {
-      //this.props.history.push(urlParts.splice(0, 2).join('/') + newUrl);
       navigate(urlParts.splice(0, 2).join('/') + newUrl);
     } else {
-      //this.props.history.push(urlParts.join('/'));
       navigate(urlParts.join('/'));
     }
   };
