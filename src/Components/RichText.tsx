ba05da3a1cf655c0eccca2ec8866f6433f8f17a5
Ondřej Chrastina <ondrejch@kontent.ai>
2023-02-01 15:24:57 +0100

    shocase the rich text reslution possibilities

--- src/Components/RichText.tsx
+++ src/Components/RichText.tsx
@@ -3,7 +3,7 @@ import {
   ResolversType,
   RichTextElement,
 } from '@kontent-ai/react-components';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { resolveContentLink } from '../Utilities/ContentLinks';
 import {
@@ -13,6 +13,10 @@ import {
   ILink,
   IRichTextImage,
 } from '@kontent-ai/delivery-sdk';
+import { RichTextResolver } from 'kontent-rich-text-to-json-converter';
+import { IOutputResult } from 'kontent-rich-text-to-json-converter/src/IResolver';
+import { useEffect } from 'react';
+import { IDomNode } from 'kontent-rich-text-to-json-converter/src/IDomNode';
 
 interface RichTextProps {
   element: Elements.RichTextElement;
@@ -20,6 +24,96 @@ interface RichTextProps {
 }
 
 const RichText: React.FC<RichTextProps> = (props) => {
+
+  const [richText, setRichText] = useState<IOutputResult<JSX.Element | string> | null>(null);
+  const [richTextResolved, setRichTextResolved] = useState<JSX.Element | null>(null);
+
+  useEffect(() => {
+    const resolver = new RichTextResolver<JSX.Element | string>();
+
+    resolver.resolveAsync({
+      value: props.element.value,
+      // other way of transformation
+      // images: props.element.images.reduce<IRichTextInput["images"]>((obj, value) => {
+      //   obj[value.imageId] = {
+      //     image_id: value.imageId,
+      //     description: value.description || "",
+      //     url: value.url,
+      //     width: value.width || 0,
+      //     height: value.height || 0
+      //   }
+      //   return obj;
+      // }, {})
+      images: Object.fromEntries(props.element.images.map(image => [image.imageId, {
+        image_id: image.imageId,
+        description: image.description,
+        url: image.url,
+        width: image.width || undefined,
+        height: image.height || undefined
+      }])),
+      links: Object.fromEntries(props.element.links.map(link => [link.linkId, {
+        codename: link.codename,
+        type: link.type,
+        url_slug: link.urlSlug
+      }])),
+      modular_content: props.element.linkedItemCodenames
+    }, {}
+    // {
+    //   resolveDomNode: ((domNode) => {
+    //     let resolvedNode: JSX.Element | string | null = null;
+    //     switch (domNode.type) {
+    //       case 'tag':
+    //         resolvedNode = React.createElement(domNode.tagName, { ...domNode.attributes })
+    //         break;
+    //       case 'text':
+    //         resolvedNode = domNode.content
+    //         break
+    //       default:
+    //         break;
+    //     }
+    //     if (resolvedNode !== null) {
+    //       return Promise.resolve(resolvedNode);
+    //     }
+    //     else {
+    //       return Promise.reject()
+    //     }
+    //   })
+    // }
+    )
+      .then((value) => {
+        debugger;
+        setRichText(value);
+
+        const link = (domNode: IDomNode): JSX.Element => {
+          if (domNode.type === 'tag') {
+            // TODO "key properies"
+            const childElements = domNode.children.map(node => link(node));
+            // TODO For non-pair element like <br/> to avoid "" error
+            const children = childElements.length === 0 ? undefined : childElements;
+
+            // It is possible to make the resolution here
+            const element = React.createElement(domNode.tagName, { ...domNode.attributes }, children);
+            return element;
+          }
+
+          if (domNode.type === 'text') {
+            // TODO - is there other way that using pseudo element?
+            return <>{domNode.content}</>
+          }
+
+          throw new Error("Undefined state")
+        }
+
+        const element = React.createElement("div", undefined, value.childrenNodes.map(link));
+        setRichTextResolved(element);
+
+      });
+  }, [props.element]);
+
+
+
+
+
   const resolvers: ResolversType = {
     resolveLinkedItem: (
       linkedItem: IContentItem | undefined,
@@ -116,6 +210,8 @@ const RichText: React.FC<RichTextProps> = (props) => {
 
   return (
     <div className={props.className}>
+      <div><pre>{JSON.stringify(richText, undefined, 2)}</pre></div>
+      {richTextResolved}
       <RichTextElement richTextElement={props.element} resolvers={resolvers} />
     </div>
   );
