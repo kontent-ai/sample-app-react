b93826ed0733bc7d19e498935f03713e1be2099e
Ondřej Chrastina <ondrejch@kontent.ai>
2023-02-13 09:59:33 +0100

    update to latest version

--- src/Components/RichText.tsx
+++ src/Components/RichText.tsx
@@ -21,8 +21,8 @@ const RichText: React.FC<RichTextProps> = (props) => {
   const [richTextContent, setRichTextContent] = useState<JSX.Element[] | null>(null);
 
   useEffect(() => {
-    const resolver = new RichTextResolver<JSX.Element | string>();
-    resolver.resolveAsync({
+    const resolver = new RichTextResolver();
+    const parseResult = resolver.parse({
       value: props.element.value,
       // other way of transformation
       // images: props.element.images.reduce<IRichTextInput["images"]>((obj, value) => {
@@ -49,116 +49,113 @@ const RichText: React.FC<RichTextProps> = (props) => {
       }])),
       modular_content: props.element.linkedItemCodenames
     })
-      .then((value) => {
-        const link = (domNode: IDomNode): JSX.Element => {
-          if (domNode.type === 'tag') {
 
-            // TODO Recursion vs. cycle
-            const childElements = domNode.children.map(node => link(node));
+    const link = (domNode: IDomNode): JSX.Element => {
+      if (domNode.type === 'tag') {
 
-            // Resolution
-            if (isLinkedItem(domNode)) {
-              const itemCode = domNode.attributes['data-codename'];
-              // TODO Provide a way to get the item by default
-              const linkedItem = props.element.linkedItems.find(item => item.system.codename === itemCode);
+        // TODO Recursion vs. cycle
+        const childElements = domNode.children.map(node => link(node));
 
-              switch (linkedItem?.system.type) {
-                case 'tweet': {
-                  // TODO test out external call
-                  let tweetLink = linkedItem?.elements.tweetLink.value;
-                  let tweetID = tweetLink.match('^.*twitter.com/.*/(\\d+)/?.*$')[1];
+        // Resolution
+        if (isLinkedItem(domNode)) {
+          const itemCode = domNode.attributes['data-codename'];
+          // TODO Provide a way to get the item by default
+          const linkedItem = props.element.linkedItems.find(item => item.system.codename === itemCode);
 
-                  let selectedTheme = linkedItem?.elements.theme.value[0].codename;
-                  selectedTheme = selectedTheme ? selectedTheme : 'light';
+          switch (linkedItem?.system.type) {
+            case 'tweet': {
+              // TODO test out external call
+              let tweetLink = linkedItem?.elements.tweetLink.value;
+              let tweetID = tweetLink.match('^.*twitter.com/.*/(\\d+)/?.*$')[1];
 
-                  setTimeout(() => {
-                    window.twttr.widgets.createTweet(
-                      tweetID,
-                      document.getElementById(`tweet${tweetID}`),
-                      {
-                        theme: selectedTheme,
-                      }
-                    );
-                  }, 100);
-                  return <div key={crypto.randomUUID().toString()} style={{ backgroundColor: 'red' }} id={`tweet${tweetID}`}>TWEET {tweetID}</div>;
-                }
-                case 'hosted_video': {
-                  if (
-                    linkedItem?.elements.videoHost.value.find(
-                      (item: ElementModels.MultipleChoiceOption) =>
-                        item.codename === 'vimeo'
-                    )
-                  ) {
-                    return (
-                      <iframe
-                        key={crypto.randomUUID().toString()}
-                        className="hosted-video__wrapper"
-                        src={`https://player.vimeo.com/video/${linkedItem.elements.videoId.value}?title=0&byline=0&portrait=0`}
-                        width="640"
-                        height="360"
-                        frameBorder="0"
-                        allowFullScreen
-                        title={`Vimeo video ${linkedItem.elements.videoId.value}`}
-                      ></iframe>
-                    );
-                  } else if (
-                    linkedItem?.elements.videoHost.value.find(
-                      (item: ElementModels.MultipleChoiceOption) =>
-                        item.codename === 'youtube'
-                    )
-                  ) {
-                    return (
-                      <iframe
-                        key={crypto.randomUUID().toString()}
-                        className="hosted-video__wrapper"
-                        width="560"
-                        height="315"
-                        src={`https://www.youtube.com/embed/${linkedItem.elements.videoId.value}`}
-                        frameBorder="0"
-                        allowFullScreen
-                        title={`Youtube video ${linkedItem.elements.videoId.value}`}
-                      ></iframe>
-                    );
-                  } else {
-                    return <div key={crypto.randomUUID().toString()}>Content item not supported</div>;
+              let selectedTheme = linkedItem?.elements.theme.value[0].codename;
+              selectedTheme = selectedTheme ? selectedTheme : 'light';
+
+              setTimeout(() => {
+                window.twttr.widgets.createTweet(
+                  tweetID,
+                  document.getElementById(`tweet${tweetID}`),
+                  {
+                    theme: selectedTheme,
                   }
-                }
-                default:
-                  return <div key={crypto.randomUUID().toString()}>Content item not supported</div>;
+                );
+              }, 100);
+              return <div key={crypto.randomUUID().toString()} style={{ backgroundColor: 'red' }} id={`tweet${tweetID}`}>TWEET {tweetID}</div>;
+            }
+            case 'hosted_video': {
+              if (
+                linkedItem?.elements.videoHost.value.find(
+                  (item: ElementModels.MultipleChoiceOption) =>
+                    item.codename === 'vimeo'
+                )
+              ) {
+                return (
+                  <iframe
+                    key={crypto.randomUUID().toString()}
+                    className="hosted-video__wrapper"
+                    src={`https://player.vimeo.com/video/${linkedItem.elements.videoId.value}?title=0&byline=0&portrait=0`}
+                    width="640"
+                    height="360"
+                    frameBorder="0"
+                    allowFullScreen
+                    title={`Vimeo video ${linkedItem.elements.videoId.value}`}
+                  ></iframe>
+                );
+              } else if (
+                linkedItem?.elements.videoHost.value.find(
+                  (item: ElementModels.MultipleChoiceOption) =>
+                    item.codename === 'youtube'
+                )
+              ) {
+                return (
+                  <iframe
+                    key={crypto.randomUUID().toString()}
+                    className="hosted-video__wrapper"
+                    width="560"
+                    height="315"
+                    src={`https://www.youtube.com/embed/${linkedItem.elements.videoId.value}`}
+                    frameBorder="0"
+                    allowFullScreen
+                    title={`Youtube video ${linkedItem.elements.videoId.value}`}
+                  ></iframe>
+                );
+              } else {
+                return <div key={crypto.randomUUID().toString()}>Content item not supported</div>;
               }
             }
-
-            // TODO For non-pair element like <br/> to avoid error
-            const children = childElements.length === 0 ? undefined : childElements;
-
-            // Fallback without resolution
-            // TODO think of the key resolution
-            const attributes = { ...domNode.attributes, key: crypto.randomUUID().toString() };
-            const element = React.createElement(domNode.tagName, attributes, children);
-            console.log(element);
-
-            return element;
+            default:
+              return <div key={crypto.randomUUID().toString()}>Content item not supported</div>;
           }
-
-          if (domNode.type === 'text') {
-            // TODO - is there other way that using pseudo element?
-            return <span key={crypto.randomUUID().toString()}>{domNode.content}</span>
-          }
-
-          throw new Error("Undefined state")
         }
 
-        // TODO keys
-        const result = value.childNodes.map(link);
-        setRichTextContent(result);
-      });
+        // TODO For non-pair element like <br/> to avoid error
+        const children = childElements.length === 0 ? undefined : childElements;
+
+        // Fallback without resolution
+        // TODO think of the key resolution
+        const attributes = { ...domNode.attributes, key: crypto.randomUUID().toString() };
+        const element = React.createElement(domNode.tagName, attributes, children);
+        console.log(element);
+
+        return element;
+      }
+
+      if (domNode.type === 'text') {
+        // TODO - is there other way that using pseudo element?
+        return <span key={crypto.randomUUID().toString()}>{domNode.content}</span>
+      }
+
+      throw new Error("Undefined state")
+    }
+
+    // TODO keys
+    const result = parseResult.childNodes.map(link);
+    setRichTextContent(result);
   }, [props.element]);
 
   return (
     <div className={props.className}>
-      {/* <pre>{JSON.stringify(richTextContent, undefined, 2)}</pre> */}
       {richTextContent}
-      {/* <RichTextElement richTextElement={props.element} resolvers={resolvers} /> */}
     </div>
   );
 };
