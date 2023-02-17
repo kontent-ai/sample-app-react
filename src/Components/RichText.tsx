05269f815036e097b862f4de521c70a211734fc8
Daniel Pokorny <danielp@kontent.ai>
2023-02-17 10:30:37 +0100

    showcase union type issue

--- src/Components/RichText.tsx
+++ src/Components/RichText.tsx
@@ -3,9 +3,8 @@ import {
   ElementModels,
   Elements,
 } from '@kontent-ai/delivery-sdk';
-import { RichTextResolver } from 'kontent-rich-text-to-json-converter';
+import { RichTextBrowserParser, IDomHtmlNode, IDomNode, isText, isUnPairedElement } from '@pokornyd/kontent-ai-rich-text-parser';
 import { useEffect } from 'react';
-import { IDomHtmlNode, IDomNode } from 'kontent-rich-text-to-json-converter/src';
 
 interface RichTextProps {
   element: Elements.RichTextElement;
@@ -21,45 +20,45 @@ const RichText: React.FC<RichTextProps> = (props) => {
   const [richTextContent, setRichTextContent] = useState<JSX.Element[] | null>(null);
 
   useEffect(() => {
-    const resolver = new RichTextResolver();
-    const parseResult = resolver.parse({
-      value: props.element.value,
-      // other way of transformation
-      // images: props.element.images.reduce<IRichTextInput["images"]>((obj, value) => {
-      //   obj[value.imageId] = {
-      //     image_id: value.imageId,
-      //     description: value.description || "",
-      //     url: value.url,
-      //     width: value.width || 0,
-      //     height: value.height || 0
-      //   }
-      //   return obj;
-      // }, {})
-      images: Object.fromEntries(props.element.images.map(image => [image.imageId, {
-        image_id: image.imageId,
-        description: image.description,
-        url: image.url,
-        width: image.width || undefined,
-        height: image.height || undefined
-      }])),
-      links: Object.fromEntries(props.element.links.map(link => [link.linkId, {
-        codename: link.codename,
-        type: link.type,
-        url_slug: link.urlSlug
-      }])),
-      modular_content: props.element.linkedItemCodenames
-    })
+    const parser = new RichTextBrowserParser();
+    const parseResult = parser.parse(props.element.value);
+      
+    //   {
+    //   value: props.element.value,
+    //   // other way of transformation
+    //   // images: props.element.images.reduce<IRichTextInput["images"]>((obj, value) => {
+    //   //   obj[value.imageId] = {
+    //   //     image_id: value.imageId,
+    //   //     description: value.description || "",
+    //   //     url: value.url,
+    //   //     width: value.width || 0,
+    //   //     height: value.height || 0
+    //   //   }
+    //   //   return obj;
+    //   // }, {})
+    //   images: Object.fromEntries(props.element.images.map(image => [image.imageId, {
+    //     image_id: image.imageId,
+    //     description: image.description,
+    //     url: image.url,
+    //     width: image.width || undefined,
+    //     height: image.height || undefined
+    //   }])),
+    //   links: Object.fromEntries(props.element.links.map(link => [link.linkId, {
+    //     codename: link.codename,
+    //     type: link.type,
+    //     url_slug: link.urlSlug
+    //   }])),
+    //   modular_content: props.element.linkedItemCodenames
+    // })
 
-    const link = (domNode: IDomNode): JSX.Element => {
+    const link = (domNode: IDomNode, index: number): JSX.Element => {
       if (domNode.type === 'tag') {
 
-        // TODO Recursion vs. cycle
-        const childElements = domNode.children.map(node => link(node));
+        const childElements = domNode.children.map(node => link(node, index));
 
         // Resolution
         if (isLinkedItem(domNode)) {
           const itemCode = domNode.attributes['data-codename'];
-          // TODO Provide a way to get the item by default
           const linkedItem = props.element.linkedItems.find(item => item.system.codename === itemCode);
 
           switch (linkedItem?.system.type) {
@@ -80,7 +79,7 @@ const RichText: React.FC<RichTextProps> = (props) => {
                   }
                 );
               }, 100);
-              return <div key={crypto.randomUUID().toString()} style={{ backgroundColor: 'red' }} id={`tweet${tweetID}`}>TWEET {tweetID}</div>;
+              return <div key={index} style={{ backgroundColor: 'red' }} id={`tweet${tweetID}`}>TWEET {tweetID}</div>;
             }
             case 'hosted_video': {
               if (
@@ -91,7 +90,7 @@ const RichText: React.FC<RichTextProps> = (props) => {
               ) {
                 return (
                   <iframe
-                    key={crypto.randomUUID().toString()}
+                    key={index}
                     className="hosted-video__wrapper"
                     src={`https://player.vimeo.com/video/${linkedItem.elements.videoId.value}?title=0&byline=0&portrait=0`}
                     width="640"
@@ -109,7 +108,7 @@ const RichText: React.FC<RichTextProps> = (props) => {
               ) {
                 return (
                   <iframe
-                    key={crypto.randomUUID().toString()}
+                    key={index}
                     className="hosted-video__wrapper"
                     width="560"
                     height="315"
@@ -120,36 +119,36 @@ const RichText: React.FC<RichTextProps> = (props) => {
                   ></iframe>
                 );
               } else {
-                return <div key={crypto.randomUUID().toString()}>Content item not supported</div>;
+                return <div key={index}>Content item not supported</div>;
               }
             }
             default:
-              return <div key={crypto.randomUUID().toString()}>Content item not supported</div>;
+              return <div key={index}>Content item not supported</div>;
           }
         }
 
-        // TODO For non-pair element like <br/> to avoid error
-        const children = childElements.length === 0 ? undefined : childElements;
+        const attributes = { ...domNode.attributes, key: index };
 
-        // Fallback without resolution
-        // TODO think of the key resolution
-        const attributes = { ...domNode.attributes, key: crypto.randomUUID().toString() };
-        const element = React.createElement(domNode.tagName, attributes, children);
+        if(isUnPairedElement(domNode)) {
+          return React.createElement(domNode.tagName, attributes);
+        }
+
+        // Fallback without resolution       
+        const element = React.createElement(domNode.tagName, attributes, childElements);
         console.log(element);
 
         return element;
       }
 
-      if (domNode.type === 'text') {
-        // TODO - is there other way that using pseudo element?
-        return <span key={crypto.randomUUID().toString()}>{domNode.content}</span>
+      if (isText(domNode)) {
+        return <span key={index}>{domNode.content}</span>
       }
 
       throw new Error("Undefined state")
     }
 
     // TODO keys
-    const result = parseResult.childNodes.map(link);
+    const result = parseResult.childNodes.map((node, index) => link(node, index));
     setRichTextContent(result);
   }, [props.element]);
 
