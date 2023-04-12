59be982471515c8a4ca97fa0ccfc97333cf94486
Ondřej Chrastina <ondrejch@kontent.ai>
2023-04-12 15:39:04 +0200

    reqrite resolution to portable text

--- src/Components/RichText.tsx
+++ src/Components/RichText.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {
+  ElementModels,
   Elements,
 } from '@kontent-ai/delivery-sdk';
-import { PortableText, toPlainText } from '@portabletext/react';
-import { IPortableTextItem, browserParse, resolveTable, transform } from '@pokornyd/kontent-ai-rich-text-parser';
+import { PortableText, PortableTextReactComponents, toPlainText } from '@portabletext/react';
+import { browserParse, resolveTable, transform } from '@pokornyd/kontent-ai-rich-text-parser';
 
 interface RichTextProps {
   element: Elements.RichTextElement;
@@ -12,19 +13,82 @@ interface RichTextProps {
 
 const RichText: React.FC<RichTextProps> = (props) => {
 
-  const portableTextComponents = {
+  const portableTextComponents: Partial<PortableTextReactComponents> = {
     types: {
-      component: (block: any) => {
-        const item = props.element.linkedItems.find(item => item.system.codename === block.value.component._ref);
-        return <div>{item?.elements.text_element.value}</div>;
+      component: (block) => {
+        const linkedItem = props.element.linkedItems.find(item => item.system.codename === block.value.component._ref);
+        const contentItemType = linkedItem ? linkedItem.system.type : '';
+
+        switch (contentItemType) {
+          case 'tweet': {
+            let tweetLink = linkedItem?.elements.tweetLink.value;
+            let tweetID = tweetLink.match('^.*twitter.com/.*/(\\d+)/?.*$')[1];
+
+            let selectedTheme = linkedItem?.elements.theme.value[0].codename;
+            selectedTheme = selectedTheme ? selectedTheme : 'light';
+
+            setTimeout(() => {
+              window.twttr.widgets.createTweet(
+                tweetID,
+                document.getElementById(`tweet${tweetID}`),
+                {
+                  theme: selectedTheme,
+                }
+              );
+            }, 100);
+
+            return <div id={`tweet${tweetID}`}></div>;
+          }
+          case 'hosted_video': {
+            if (
+              linkedItem?.elements.videoHost.value.find(
+                (item: ElementModels.MultipleChoiceOption) =>
+                  item.codename === 'vimeo'
+              )
+            ) {
+              return (
+                <iframe
+                  className="hosted-video__wrapper"
+                  src={`https://player.vimeo.com/video/${linkedItem.elements.videoId.value}?title=0&byline=0&portrait=0`}
+                  width="640"
+                  height="360"
+                  frameBorder="0"
+                  allowFullScreen
+                  title={`Vimeo video ${linkedItem.elements.videoId.value}`}
+                ></iframe>
+              );
+            } else if (
+              linkedItem?.elements.videoHost.value.find(
+                (item: ElementModels.MultipleChoiceOption) =>
+                  item.codename === 'youtube'
+              )
+            ) {
+              return (
+                <iframe
+                  className="hosted-video__wrapper"
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${linkedItem.elements.videoId.value}`}
+                  frameBorder="0"
+                  allowFullScreen
+                  title={`Youtube video ${linkedItem.elements.videoId.value}`}
+                ></iframe>
+              );
+            } else {
+              return <div>Content item not supported</div>;
+            }
+          }
+          default:
+            return <div>Content item not supported</div>;
+        }
       },
-      table: ({ value }: any) => {
-        let tableString = resolveTable(value, toPlainText);
+      table: ({ value }) => {
+        const tableString = resolveTable(value, toPlainText);
         return <>{tableString}</>;
       }
     },
     marks: {
-      link: ({ value, children }: any) => {
+      link: ({ value, children }) => {
         const target = (value?.href || '').startsWith('http') ? '_blank' : undefined
         return (
           <a href={value?.href} target={target} rel={value?.rel} title={value?.title} data-new-window={value['data-new-window']}>
@@ -32,7 +96,7 @@ const RichText: React.FC<RichTextProps> = (props) => {
           </a>
         )
       },
-      internalLink: ({ value, children }: any) => {
+      internalLink: ({ value, children }) => {
         const item = props.element.linkedItems.find(item => item.system.id === value.reference._ref);
         return (
           <a href={"https://somerandomwebsite.xyz/" + item?.system.codename}>
@@ -48,7 +112,7 @@ const RichText: React.FC<RichTextProps> = (props) => {
 
   return (
     <div className={props.className}>
-      {/* <PortableText value={portableText} components={portableTextComponents} /> */}
+      <PortableText value={portableText} components={portableTextComponents} />
     </div>
   );
 };
